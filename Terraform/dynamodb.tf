3f80a770a197e6a9535bd1e90f636dbd518867ca
JaimeCardona06 <jaime.cardona40549@ucaldas.edu.co>
2024-11-15 10:38:11 -0500

    .gitignore update

--- Terraform/dynamodb.tf
+++ Terraform/dynamodb.tf
@@ -1,6 +1,6 @@
 resource "aws_dynamodb_table" "csv_table" {
   name         = "movies"
-  hash_key     = "id"  # Asegúrate de usar un campo único para el hash_key
+  hash_key     = "id"
   read_capacity  = 5
   write_capacity = 5
 
